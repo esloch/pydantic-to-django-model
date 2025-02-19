a79176cbaac24a90ccd9b8c31d51da3b8d2c4678
Sandro Loch <es.loch@gmail.com>
2025-02-18 21:10:30 -0300

    feat(linter): Install pre-commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 """Tests for pydantic_to_django_model package."""
+
 import pytest
 
 from hypothesis import given
@@ -6,23 +7,23 @@ from hypothesis import strategies as st
 
 
 @pytest.fixture
-def response_pytest():
+def response_pytest() -> bool:
     """Sample pytest fixture."""
     return True
 
 
 @pytest.fixture
-def response_hypothesis():
+def response_hypothesis() -> bool:
     """Sample pytest + hypothesis fixture."""
     return True
 
 
-def test_content_pytest():
+def test_content_pytest() -> None:
     """Test with pytest."""
-    assert True
+    assert True is True
 
 
-@given(st.text().filter(lambda s: s != ""))
-def test_content_hypothesis(response_hypothesis):
+@given(st.text().filter(lambda s: s != ''))
+def test_content_hypothesis(response_hypothesis: bool) -> None:
     """Test with pytest + hypothesis."""
     assert response_hypothesis
